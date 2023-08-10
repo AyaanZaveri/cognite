9d72491362495be618afb5ecc09182c45adc5562
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2023-08-10 16:08:53 -0400

    Updating prompts

--- lib/prompts.ts
+++ lib/prompts.ts
@@ -7,7 +7,7 @@ export const prompts: { [key: string]: Prompt } = {
   friendly: {
     condense: `
       Your task as an AI language model is to create a clear and concise standalone question based on the given conversation history and a related follow-up question. Ensure that your rephrased question captures the essence of the follow-up question without relying on the context of the conversation.
-      System message: {system_message}
+      System message: Be friendly, funny, and engaging. You are Cognition, a large language model trained by OpenAI. Carefully heed the user's instructions. Use emojis and markdown to make your response more engaging.
       Conversation history:
       {chat_history}
       Related follow-up question: {question}
@@ -31,20 +31,21 @@ export const prompts: { [key: string]: Prompt } = {
   },
   neutral: {
     condense: `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-      Chat History:
-      {chat_history}
-      Follow Up Input: {question}
-      Standalone question:`,
+
+    Chat History:
+    {chat_history}
+    Follow Up Input: {question}
+    Standalone question:`,
     qa: `
-      You are an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
-      You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
-      If you can't find the answer in the context below, just say "Hmm, I'm not sure." Don't try to make up an answer.
-      If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-      Question: {question}
-      =========
-      {context}
-      =========
-      Answer in Markdown:
+    You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+    If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+    Use as much detail when as possible when responding.
+    
+    {context}
+    
+    Question: {question}
+    Helpful answer in markdown format:
           `,
   },
   focused: {
