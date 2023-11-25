dd7d9f6460edd5bd496438a0eb817dce9537c1dd
AyaanZaveri <ayaanzaveri08@gmail.com>
2023-11-24 23:29:45 -0500

    fixing prompts

--- lib/prompts.ts
+++ lib/prompts.ts
@@ -29,13 +29,14 @@ export const prompts: { [key: string]: Prompt } = {
   `,
   },
   neutral: {
-    condense: `Given the following conversation and a follow up question, rephrase the follow up question to be a 
-    standalone question without changing the content in given question.
+    condense: `
+    Given the following conversation and a follow up question, enhance the follow up question to be a standalone question.
     
     Chat History:
     {chat_history}
     Follow Up Input: {question}
-    Standalone question:`,
+    Standalone question:
+          `,
     qa: `
   Carefully heed the user's instructions. 
   Respond using Markdown.
@@ -55,7 +56,8 @@ export const prompts: { [key: string]: Prompt } = {
     Chat History:
     {chat_history}
     Follow Up Input: {question}
-    Standalone question:`,
+    Standalone question:
+          `,
     qa: `
     Carefully heed the user's instructions. 
 
